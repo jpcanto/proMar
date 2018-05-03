615f9ffcc1447e9398b6ca93bddc80d6245b8dc2
joao <joao.canto@medbarra.com.br>
2018-05-03 19:44:59 -0300

    alterando gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp');
-var sass = require('node-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('./sass/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./css'));
 });
 
 gulp.task('automatizaTaskSass', function() {
-    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('./sass/**/*.scss', ['sass']);
 })
