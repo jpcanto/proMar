6acfb5c2fec5fec21cc81bd456c7b545733bbb28
joao <joao.canto@medbarra.com.br>
2018-05-09 16:32:49 -0300

    criando task server

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
     gulp.src('./sass/**/*.scss')
@@ -7,6 +8,17 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('automatizaTaskSass', function() {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+//gulp.task('automatizaTaskSass', function() {
+//gulp.watch('./sass/**/*.scss', ['sass']);
+//}) //a task browser sync já faz o watch e transforma em css
+
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch('./css/*.css').on('change', browserSync.reload)
+    gulp.watch('./sass/**/*.scss', ['sass'])
 })
