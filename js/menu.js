a738ad553e89ae349aee115648e2b1227f8a1f1e
joao <joao.canto@medbarra.com.br>
2018-05-14 16:20:52 -0300

    ajuste do hamburger js

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,29 @@
 ;
 (function() {
     'use strict';
-    var btn = document.querySelector('mainNavigation__hamburgger');
+
+    var btn = document.querySelector('.mainNavigation__hamburgger');
     var html = document.querySelector('html');
     var menuOpened = false;
 
-    function(closeMenu) {
+    btn.addEventListener('click', toggleMenu);
+
+    function toggleMenu() {
+        if (menuOpened) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    }
+
+    function closeMenu() {
         menuOpened = false;
         html.classList.remove('menu-opened');
     }
 
-    function(openMenu) {
-        menuOpened = false;
+    function openMenu() {
+        menuOpened = true;
         html.classList.add('menu-opened');
-    }
-
-    function(toggleMenu) {
-        if (menuOpened) {
-
-        }
     }
 
 }())
